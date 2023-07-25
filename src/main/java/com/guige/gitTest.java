166a447ea3c14b2ca0c71ddf07c241fc2547b8ad
shinezone <shinezone@qq.com>
2023-07-25 16:31:35 +0800

    hot-fix test2

--- src/main/java/com/guige/gitTest.java
+++ src/main/java/com/guige/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("hello git!2");
         System.out.println("hello git!3");
         System.out.println("hot-fix");
+        System.out.println("hot-fix222222");
     }
 }
