7aa5acbaeb35bbd437da9fa5fd1c28968335133b
shinezone <shinezone@qq.com>
2023-07-25 16:25:06 +0800

    first commit

---
+++ src/main/java/com/guige/gitTest.java
@@ -0,0 +1,9 @@
+package com.guige;
+
+import org.apache.wicket.markup.transformer.XsltOutputTransformerContainer;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
