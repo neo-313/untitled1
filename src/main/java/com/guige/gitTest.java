51cc809adc486568e19c4ea241486d27c3a34e04
shinezone <shinezone@qq.com>
2023-07-25 16:25:48 +0800

    second

--- src/main/java/com/guige/gitTest.java
+++ src/main/java/com/guige/gitTest.java
@@ -5,5 +5,6 @@ import org.apache.wicket.markup.transformer.XsltOutputTransformerContainer;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
