3a1bfb53ec6db698aa541420bc80dc35524b7b25
shinezone <shinezone@qq.com>
2023-07-25 16:26:20 +0800

    third

--- src/main/java/com/guige/gitTest.java
+++ src/main/java/com/guige/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!2");
+        System.out.println("hello git!3");
     }
 }
